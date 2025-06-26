7d4323d994dd19561a14a31f030133c01c9fe584
Sahil Padariya <sahilpa24@gmail.com>
2025-06-26 10:26:21 +0530

    Update WhatsappMsg.java

--- src/main/java/com/billGenerate/BillGenerate/MassageService/WhatsappMsg.java
+++ src/main/java/com/billGenerate/BillGenerate/MassageService/WhatsappMsg.java
@@ -7,29 +7,29 @@ public class WhatsappMsg {
 
     public void UpdateMassage(){
         Message.creator(
-                new PhoneNumber("whatsapp:+917990747312"), // TO: Receiver+
-                new PhoneNumber("whatsapp:+14155238886"),// FROM: Twilio Sandbox number
+                new PhoneNumber("whatsapp:+****"), // TO: Receiver+
+                new PhoneNumber("whatsapp:+****"),// FROM: Twilio Sandbox number
                 "please Update Stock"
         ).create();
     }
     public void OutOfStoc(){
         Message.creator(
-                new PhoneNumber("whatsapp:+917990747312"), // TO: Receiver+
-                new PhoneNumber("whatsapp:+14155238886"),                 // FROM: Twilio Sandbox number
+                new PhoneNumber("whatsapp:+****"), // TO: Receiver+
+                new PhoneNumber("whatsapp:+****"),                 // FROM: Twilio Sandbox number
                 "We not Enough Stock for this product" // ✅ message string + variable
         ).create();
     }
     public void SuccessFullyBuy(){
         Message.creator(
-                new PhoneNumber("whatsapp:+917990747312"), // TO: Receiver+
-                new PhoneNumber("whatsapp:+14155238886"),                 // FROM: Twilio Sandbox number
+                new PhoneNumber("whatsapp:+****"), // TO: Receiver+
+                new PhoneNumber("whatsapp:+****"),                 // FROM: Twilio Sandbox number
                 "You have successfully bought:"
         ).create();
     }
     public void UnsuccessBUy(){
         Message.creator(
-                new PhoneNumber("whatsapp:+917990747312"), // TO: Receiver+
-                new PhoneNumber("whatsapp:+14155238886"),                 // FROM: Twilio Sandbox number
+                new PhoneNumber("whatsapp:+****"), // TO: Receiver+
+                new PhoneNumber("whatsapp:+****"),                 // FROM: Twilio Sandbox number
                 "payment fail" // ✅ message string + variable
         ).create();
     }
